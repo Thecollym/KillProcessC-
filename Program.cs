6720ad9691648fedcfd4907a70addcaceecf007b
/Luis R Collymoore <lcollymoore@gmail.com>
2024-07-26 10:51:50 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+class StopTask
+{ 
+    static void Main(string[] args)
+    {
+        List<string> list = new List<string>();
+        processName = ["msedge","brave"];
+     
+
+        foreach (var process in Process.GetProcessesByName(processName))
+        {
+            try
+            {
+                process.Kill();
+                Console.WriteLine($"Proceso {processName} (ID: {process.Id}) detenido.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"No se pudo detener el proceso {processName} (ID: {process.Id}): {ex.Message}");
+            }
+        }
+    }
+}
